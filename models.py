47b8c34d94ee579ae890c2c2c1e131ce671b1aa2
saelsa <saelsa@gmx.net>
2019-10-14 20:41:00 +0200

    feat: Model definition for Artist, Venue and Show

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    shows = db.relationship('show', backref="artist", lazy=True)
+    shows = db.relationship('Show', backref="artist", lazy=True)
 
     def __repr__(self):
         return '<Artist {}>'.format(self.name)
